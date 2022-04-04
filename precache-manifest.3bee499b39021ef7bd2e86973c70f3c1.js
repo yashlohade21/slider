04e9ead86e1e4b3c79745691a0394c233e7de8a2
Yashlohade21 <yashlohade125@gmail.com>
2022-04-04 22:54:42 +0530

    Updates

---
+++ precache-manifest.3bee499b39021ef7bd2e86973c70f3c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8872d32fdbbeb8fa4a3f4df1feb273bd",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "1a98255792ffcc37197c",
+    "url": "/slider/static/css/main.9648fb74.chunk.css"
+  },
+  {
+    "revision": "4acce97ee23a5c46634a",
+    "url": "/slider/static/js/2.6e9d5766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider/static/js/2.6e9d5766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a98255792ffcc37197c",
+    "url": "/slider/static/js/main.3a6c20d7.chunk.js"
+  },
+  {
+    "revision": "ce1caf34cf0aba241fa2",
+    "url": "/slider/static/js/runtime-main.c6fd19b8.js"
+  }
+]);
